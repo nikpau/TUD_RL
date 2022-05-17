1f0ebc2291ff88d46ce35db9abeeab705e1e4866
nikpau <n.paulig@web.de>
2022-05-17 11:02:28 +0200

    progress plot design

--- tud_rl/common/logging_plot.py
+++ tud_rl/common/logging_plot.py
@@ -1,10 +1,16 @@
 import csv
+import os
 
 #matplotlib.use("agg")
 import matplotlib.pyplot as plt
 import pandas as pd
-from tud_rl.common.helper_fnc import exponential_smoothing
+from .helper_fnc import exponential_smoothing
+from collections import namedtuple
+from pathlib import Path
 
+# Green, brown
+COLORS = namedtuple("COLORS", ["green","yellow","brown","bluish","Cream"])
+colors = COLORS("#205855","#FF7D00","#6C534E","#8DA9C4", "#EEF4ED")
 
 def plot_from_progress(dir, alg, env_str, info=None):
     """Plots based on a given 'progress.txt' the evaluation return, Q_values and losses.
@@ -15,6 +21,11 @@ def plot_from_progress(dir, alg, env_str, info=None):
         alg (string):     used algorithm
         info (string):    further information to display in the header
     """
+
+    # Load logo
+    dirname = Path(os.path.dirname(__file__))
+    logo = plt.imread(f"{dirname.parents[0]}/assets/logo.png")
+
     # open progress file and load it into pandas
     with open(f"{dir}/progress.txt") as f:
         reader = csv.reader(f, delimiter="\t")
@@ -37,38 +48,54 @@ def plot_from_progress(dir, alg, env_str, info=None):
         fig.suptitle(f"{alg} | {env_str} | Runtime (h): {runtime}")
 
     # first axis
-    ax[0,0].plot(df["Timestep"], df["Avg_Eval_ret"], label = "Avg. test return")
-    ax[0,0].plot(df["Timestep"], exponential_smoothing(df["Avg_Eval_ret"].values), label = "Exp. smooth. return")
+    ax[0,0].plot(
+        df["Timestep"], 
+        df["Avg_Eval_ret"], 
+        label = "Avg. test return",
+        color = colors.green
+    )
+    ax[0,0].plot(
+        df["Timestep"], 
+        exponential_smoothing(df["Avg_Eval_ret"].values), 
+        label = "Exp. smooth. return",
+        color = colors.yellow
+    )
     ax[0,0].legend()
     ax[0,0].set_xlabel("Timestep")
     ax[0,0].set_ylabel("Test return")
 
     # second axis
     if "Avg_Q_val" in df.columns:
-        ax[0,1].plot(df["Timestep"], df["Avg_Q_val"])
+        ax[0,1].plot(df["Timestep"], df["Avg_Q_val"],color = colors.green)
         ax[0,1].set_ylabel("Avg_Q_val")
         ax[0,1].set_xlabel("Timestep")
     
     # third axis
     if "Loss" in df.columns:
-        ax[1,0].plot(df["Timestep"], df["Loss"])
+        ax[1,0].plot(df["Timestep"], df["Loss"],color = colors.green)
         ax[1,0].set_xlabel("Timestep")
         ax[1,0].set_ylabel("Loss")
 
     if "Critic_loss" in df.columns and "Actor_loss" in df.columns:
-        ax[1,0].plot(df["Timestep"], df["Critic_loss"], label="Critic")
-        ax[1,0].plot(df["Timestep"], df["Actor_loss"], label="Actor")
+        ax[1,0].plot(df["Timestep"], df["Critic_loss"], label="Critic", color = colors.brown)
+        ax[1,0].plot(df["Timestep"], df["Actor_loss"], label="Actor", color = colors.bluish)
         ax[1,0].legend()
 
     # fourth axis
     ax[1,1].set_xlabel("Timestep")
     
     if all(ele in df.columns for ele in ["Avg_bias", "Std_bias", "Max_bias", "Min_bias"]):
-        ax[1,1].plot(df["Timestep"], df["Avg_bias"], label="Avg. bias")
-        ax[1,1].plot(df["Timestep"], df["Std_bias"], label="Std. bias")
-        ax[1,1].plot(df["Timestep"], df["Max_bias"], label="Max. bias")
-        ax[1,1].plot(df["Timestep"], df["Min_bias"], label="Min. bias")
+        ax[1,1].plot(df["Timestep"], df["Avg_bias"], label="Avg. bias",color = colors.green)
+        ax[1,1].plot(df["Timestep"], df["Std_bias"], label="Std. bias",color = colors.yellow)
+        ax[1,1].plot(df["Timestep"], df["Max_bias"], label="Max. bias",color = colors.brown)
+        ax[1,1].plot(df["Timestep"], df["Min_bias"], label="Min. bias",color = colors.bluish)
         ax[1,1].legend()
+
+    
+    # Add logo to plot
+    logoax = fig.add_axes([0.7,0.72,0.2,0.2], anchor='NE', zorder=1)
+    logoax.imshow(logo)
+    logoax.axis('off')
     
     # safe figure and close
     plt.savefig(f"{dir}/{alg}_{env_str}.pdf")
